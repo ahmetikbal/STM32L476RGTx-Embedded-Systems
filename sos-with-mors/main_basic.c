7a29956090308449029df3e52df45f179cc7b336
Ahmet İkbal Adlığ <a-ikbal@hotmail.com>
2025-11-18 16:12:48 +0300

    LED and button pin configurations using GPIO ports

--- sos-with-mors/main_basic.c
+++ sos-with-mors/main_basic.c
@@ -1,6 +1,66 @@
 #include "stm32l476xx.h"
 
+// PA.5  <--> Green LED
+// PC.13 <--> Blue user button
+#define LED_PIN    5
+#define BUTTON_PIN 13
+
+
+// User HSI (high-speed internal) as the processor clock
+void enable_HSI(){
+	// Enable High Speed Internal Clock (HSI = 16 MHz)
+  RCC->CR |= ((uint32_t)RCC_CR_HSION);
+	
+  // wait until HSI is ready
+  while ( (RCC->CR & (uint32_t) RCC_CR_HSIRDY) == 0 ) {;}
+	
+  // Select HSI as system clock source 
+  RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+  RCC->CFGR |= (uint32_t)RCC_CFGR_SW_HSI;  //01: HSI16 oscillator used as system clock
+
+  // Wait till HSI is used as system clock source 
+  while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) == 0 ) {;}
+}
+
+
+void configure_LED_pin(){
+	// PA.5  <--> Green LED
+  // Enable the clock to GPIO Port A
+	//RCC->AHB2ENR |= (1<<2);	
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;   
+		
+	// GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+	GPIOA->MODER &= ~(3UL<<(2*LED_PIN));  
+	GPIOA->MODER |=   1UL<<(2*LED_PIN);      // Output(01)
+	
+	// GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+	GPIOA->OSPEEDR &= ~(3<<(2*LED_PIN));
+	GPIOA->OSPEEDR |=   2<<(2*LED_PIN);  // Fast speed
+	
+	// GPIO Output Type: Output push-pull (0, reset), Output open drain (1) 
+	GPIOA->OTYPER &= ~(1<<LED_PIN);      // Push-pull
+	
+	// GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+	GPIOA->PUPDR  &= ~(3<<(2*LED_PIN));  // No pull-up, no pull-down
+}
+
+
+void configure_button_pin(){
+	// PC.13 <--> Blue user button
+  // Enable the clock to GPIO Port C	
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;   
+		
+	// GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+  GPIOC->MODER &= ~(3UL<<26);              // Input(00)
+	
+	// GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+  GPIOC->PUPDR &= ~(3UL<<26);              // No pull-up, no pull-down
+}
+
+
 
 int main(void){
-
+	enable_HSI();
+	configure_LED_pin();
+	configure_button_pin();
 }
